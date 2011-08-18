916e2131f8b4d1911223d010e2a70cffcac7c841
moritz-gitorious@twoticketsplease.de <moritz-gitorious@twoticketsplease.de>
2011-08-18 15:10:33 +0000

    add group to composition tests

--- tests/composition-test.scm
+++ tests/composition-test.scm
@@ -1,13 +1,14 @@
 (use test)
 (import ssql)
 
-(test "simple"
-  '(select (columns firstname lastname) (from artists) (where (= firstname "Frank")))
-  (ssql-compose #f
-                '(select (columns firstname lastname) (from artists))
-                '((where (= firstname "Frank")))))
+(test-group "composition"
+  (test "simple"
+    '(select (columns firstname lastname) (from artists) (where (= firstname "Frank")))
+    (ssql-compose #f
+                  '(select (columns firstname lastname) (from artists))
+                  '((where (= firstname "Frank")))))
 
-(test "merge"
-  '(select (columns firstname lastname age) (from artists) (order firstname lastname))
-  (ssql-compose #f '(select (columns firstname lastname) (order firstname))
-                '((columns age) (from artists) (order lastname))))
+  (test "merge"
+    '(select (columns firstname lastname age) (from artists) (order firstname lastname))
+    (ssql-compose #f '(select (columns firstname lastname) (order firstname))
+                  '((columns age) (from artists) (order lastname)))))
